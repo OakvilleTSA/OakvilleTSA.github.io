9bd1a5185b0303385a9cef19bcad713a4b707d69
Garrett Summerfield <weatherfreak10@gmail.com>
2017-11-01 12:16:17 -0500

    tweaking more things

--- jsbgdots.js
+++ jsbgdots.js
@@ -1,8 +1,8 @@
 var canvasDots = function() {
     var canvas = document.querySelector('canvas'),
         ctx = canvas.getContext('2d'),
-        colorDot = '#7A7A7A',
-        color = '#7A7A7A';
+        colorDot = '#212121',
+        color = '#212121';
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.style.display = 'block';
